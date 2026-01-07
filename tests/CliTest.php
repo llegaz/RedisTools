547fffffd74bf7b916f701e9f525f7173ecfc46a
Laurent Legaz <laurent@legaz.eu>
2026-01-07 16:20:40 +0100

    CS

--- tests/CliTest.php
+++ tests/CliTest.php
@@ -50,5 +50,5 @@ $inspector->dumpAllRedis(false, 0, 2);
 $inspector->dumpCachePool();*/
 
 
-echo "From CliTest.php: adapter used> " . $inspector->getRedis() . PHP_EOL;
-echo "See you space cowboy..." . PHP_EOL;
+echo 'From CliTest.php: adapter used> ' . $inspector->getRedis() . PHP_EOL;
+echo 'See you space cowboy...' . PHP_EOL;
